3a61a8f8d1aaca442b7eacd62845c860418740d2
Dipanshu Varshney <96021776+Dipanshu1210@users.noreply.github.com>
2022-10-07 22:53:26 +0530

    Add files via upload

---
+++ Question 4/Q4.js
@@ -0,0 +1,11 @@
+function func1() {
+    let em = document.forms["myform"]["email"].value;
+    if (em == "") {
+        alert("Enter a valid Email");
+        return false;
+    }
+    else {
+        alert("You Email found in Database");
+        return true;
+    }
+}
